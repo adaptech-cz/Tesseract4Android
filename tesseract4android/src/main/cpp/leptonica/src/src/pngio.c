0f0e072e122e79d7708210c6b1c544ff898313c5
Robert Pösel <posel@adaptech.cz>
2019-07-17 10:58:29 +0200

    Leptonica: Fix from alexcohn to have consistent alpha value
    See: https://github.com/DanBloomberg/leptonica/issues/442
    
    Android sets alpha value to 255 when loading images without alpha channel into ARGB_8888 format, but Leptonica keeps alpha value unset (or 0). This commit sets alpha value to 255 in Leptonica to make data of jpeg/png images consistent with data on Android.
    
    LIBRARY CHANGE

--- tesseract4android/src/main/cpp/leptonica/src/src/pngio.c
+++ tesseract4android/src/main/cpp/leptonica/src/src/pngio.c
@@ -330,6 +330,8 @@ PIXCMAP     *cmap;
                 SET_DATA_BYTE(ppixel, COLOR_BLUE, rowptr[k++]);
                 if (spp == 4)
                     SET_DATA_BYTE(ppixel, L_ALPHA_CHANNEL, rowptr[k++]);
+                else /* spp == 3 */
+                    SET_DATA_BYTE(ppixel, L_ALPHA_CHANNEL, 255);
                 ppixel++;
             }
         }
