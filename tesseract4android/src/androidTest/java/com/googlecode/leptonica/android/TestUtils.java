1a03d7719d8e255eb4ed1368efef8bf292ab62f0
Robert Pösel <posel@adaptech.cz>
2019-07-17 18:13:36 +0200

    Improve image comparison methods
    Added logging to see pixel differences and added parameter to specify tolerance, which will ignore changes between two pixels at the level of color channel.
    
    E.g., tolerance=10 means that for example these colors are considered the same: Color.argb(0, 50, 80, 250) and Color.argb(10, 40, 75, 255);

--- tesseract4android/src/androidTest/java/com/googlecode/leptonica/android/TestUtils.java
+++ tesseract4android/src/androidTest/java/com/googlecode/leptonica/android/TestUtils.java
@@ -23,6 +23,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Rect;
+import android.util.Log;
+
+import androidx.annotation.ColorInt;
 
 /**
  * Utility methods for running Leptonica unit tests.
@@ -31,13 +34,25 @@ import android.graphics.Rect;
  */
 @SuppressWarnings("WeakerAccess")
 public class TestUtils {
+	private static final String TAG = TestUtils.class.getSimpleName();
+	private static final boolean LOG_DIFFERENCES = false;
+
 	public static float compareBitmaps(Bitmap a, Bitmap b) {
+		return compareBitmaps(a, b, 0);
+	}
+
+	public static float compareBitmaps(Bitmap a, Bitmap b, int tolerance) {
 		int found = 0;
 
 		for (int y = 0; y < a.getHeight(); y++) {
 			for (int x = 0; x < a.getWidth(); x++) {
-				if (a.getPixel(x, y) == b.getPixel(x, y)) {
+				int colorA = a.getPixel(x, y);
+				int colorB = b.getPixel(x, y);
+
+				if (compareColors(colorA, colorB, tolerance)) {
 					found++;
+				} else if (LOG_DIFFERENCES) {
+					Log.v(TAG, String.format("compareBitmaps: Different pixel at [%d, %d]: a=%08X != b=%08X", x, y, colorA, colorB));
 				}
 			}
 		}
@@ -46,12 +61,21 @@ public class TestUtils {
 	}
 
 	public static float compareImages(Pix a, Bitmap b) {
+		return compareImages(a, b, 0);
+	}
+
+	public static float compareImages(Pix a, Bitmap b, int tolerance) {
 		int found = 0;
 
 		for (int y = 0; y < a.getHeight(); y++) {
 			for (int x = 0; x < a.getWidth(); x++) {
-				if (a.getPixel(x, y) == b.getPixel(x, y)) {
+				int colorA = a.getPixel(x, y);
+				int colorB = b.getPixel(x, y);
+
+				if (compareColors(colorA, colorB, tolerance)) {
 					found++;
+				} else if (LOG_DIFFERENCES) {
+					Log.v(TAG, String.format("compareImages: Different pixel at [%d, %d]: a=%08X != b=%08X", x, y, colorA, colorB));
 				}
 			}
 		}
@@ -60,17 +84,46 @@ public class TestUtils {
 	}
 
 	public static float comparePix(Pix a, Pix b) {
+		return comparePix(a, b, 0);
+	}
+
+	public static float comparePix(Pix a, Pix b, int tolerance) {
 		int found = 0;
 
 		for (int y = 0; y < a.getHeight(); y++) {
 			for (int x = 0; x < a.getWidth(); x++) {
-				if (a.getPixel(x, y) == b.getPixel(x, y)) {
+				int colorA = a.getPixel(x, y);
+				int colorB = b.getPixel(x, y);
+
+				if (compareColors(colorA, colorB, tolerance)) {
 					found++;
+				} else if (LOG_DIFFERENCES) {
+					Log.v(TAG, String.format("comparePix: Different pixel at [%d, %d]: a=%08X != b=%08X", x, y, colorA, colorB));
 				}
 			}
 		}
 
 		return found / (float) (a.getWidth() * a.getHeight());
+	}
+
+	private static boolean compareColors(@ColorInt int color1, @ColorInt int color2, int tolerance) {
+		if (tolerance == 0) {
+			return color1 == color2;
+		}
+		int r1 = Color.red(color1);
+		int g1 = Color.green(color1);
+		int b1 = Color.blue(color1);
+		int a1 = Color.alpha(color1);
+
+		int r2 = Color.red(color2);
+		int g2 = Color.green(color2);
+		int b2 = Color.blue(color2);
+		int a2 = Color.alpha(color2);
+
+		return Math.abs(r1 - r2) <= tolerance
+				&& Math.abs(g1 - g2) <= tolerance
+				&& Math.abs(b1 - b2) <= tolerance
+				&& Math.abs(a1 - a2) <= tolerance;
 	}
 
 	public static Bitmap createTestBitmap(int width, int height, Bitmap.Config format) {
